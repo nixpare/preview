942dd8899864566ead9a988fe1c1e2766898cfaa
Alessio Pareto (MacBook Pro di Pare) <alessio.pareto@nixpare.com>
2024-11-27 17:43:40 +0100

    Improved chat styling

--- src/components/ServerChat.tsx
+++ src/components/ServerChat.tsx
@@ -1,7 +1,6 @@
-import './ServerLogs.css'
 import './ServerChat.css'
 
-import { useEffect, useState, MouseEvent, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import SendCommand from './SendCommand';
 import { ChatMessage, ParsedLog } from '../models/Logs';
 import { User } from '../models/User';
@@ -21,7 +20,7 @@ export default function ServerChat({ serverName, chat, show, showMessage }: Serv
     useEffect(() => {
         if (!scrollAtBottom)
             return
-        
+
         serverChatEl.current?.scroll({ top: serverChatEl.current.scrollHeight, behavior: 'smooth' })
         setScrollAtBottom(true)
     }, [chat])
@@ -57,27 +56,16 @@ export default function ServerChat({ serverName, chat, show, showMessage }: Serv
         if (response.status >= 400)
             showMessage(response.data);
     }
-    
+
     return (
-        <div className="server-chat" style={!show ? { display: 'none'} : undefined}>
+        <div className="server-chat" style={!show ? { display: 'none' } : undefined}>
             <div className="send-broadcast">
                 <SendCommand label="Broadcast Message" sendFunc={sendBroadcast} />
             </div>
-            <div className="server-logs" onScroll={onScroll} ref={serverChatEl}>
-                <table>
-                    <thead>
-                        <tr>
-                            <th scope="col">TIME</th>
-                            <th scope="col">FROM</th>
-                            <th scope="col">MESSAGE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {chat.map(message => (
-                            <Message key={message.id} message={message} />
-                        ))}
-                    </tbody>
-                </table>
+            <div className="chat" onScroll={onScroll} ref={serverChatEl}>
+                {chat.map(message => (
+                    <Message key={message.id} message={message} />
+                ))}
             </div>
             <div className="send-message">
                 <SendCommand label="Message" sendFunc={sendMessage} />
@@ -87,45 +75,33 @@ export default function ServerChat({ serverName, chat, show, showMessage }: Serv
 }
 
 function Message({ message }: { message: ChatMessage }) {
-    const multiline = message.message.includes('\n') ? 'multiline' : ''
-
-    const [showing, setShowing] = useState(false)
-    const [profilePicture, setProfilePicture] = useState<any>(null)
-    const toggleShowing = (ev: MouseEvent) => {
-        ev.preventDefault()
-        setShowing(!showing)
-    }
-
+    const [profilePicture, setProfilePicture] = useState<string | null>(null)
     useEffect(() => {
-        axios.get(`/profile/${message.username}`, { responseType: 'blob' })
-        .then(response => {
-            console.log(response)
-            setProfilePicture(URL.createObjectURL(response.data) )
-        });
+        axios.get(`/profile/${message.from}`, { responseType: 'blob' })
+            .then(response => {
+                setProfilePicture(URL.createObjectURL(response.data))
+            });
     }, []);
+    
+    let date = message.date.slice(message.date.indexOf('\n') + 1)
+    date = date.slice(0, date.lastIndexOf(':'))
 
-    return <tr>
-        <td>{message.date}</td>
-        <td>{profilePicture && <img src={profilePicture} width={50}/>} {message.from}</td>
-        <td>
-            <div className={`log-message ${multiline} ${showing ? 'show' : ''}`} onClick={toggleShowing}>
-                <div className="message">
-                    {message.message}
-                </div>
-                <div className="expand">
-                    <i className="fa-solid fa-chevron-right"></i>
-                </div>
-            </div>
-        </td>
-    </tr>
+    return <div className={`message ${message.self ? 'self' : ''}`}>
+        <div className="from">
+            {!message.self && profilePicture && <img src={profilePicture} />}
+            <div>{message.from}</div>
+        </div>
+        <div className="content">{message.message}</div>
+        <div className="date">{date}</div>
+    </div>
 }
 
 export function parseChatMessage(user: User, log: ParsedLog, chat: ChatMessage[], players: User[]) {
     let message = log.message
-    if (log.tags?.includes('chat')){
-        message = message.slice(message.indexOf('\n')+1)
+    if (log.tags?.includes('chat')) {
+        message = message.slice(message.indexOf('\n') + 1)
     }
-    
+
     if (!message.trim().startsWith('<'))
         return
 
@@ -136,13 +112,9 @@ export function parseChatMessage(user: User, log: ParsedLog, chat: ChatMessage[]
         // if an online player is not found it is NOT a message
         return
 
-    if (from == user.name) {
-        from = 'You'
-    }
-
     chat.push({
         id: log.id, date: log.date,
         from: from, message: message,
-        username: user.name
+        self: from == user.name
     });
 }
--- src/models/Logs.tsx
+++ src/models/Logs.tsx
@@ -25,7 +25,7 @@ export type ParsedLog = {
 export type ChatMessage = {
 	id: string
 	date: string
-	from: string	// it can take the value 'you' if the message is from the user
-	username: string
+	from: string
 	message: string
+	self: boolean
 }
