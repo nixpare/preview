8922d7a0233a5169926ee47a18276702fc4d301a
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2024-11-29 17:07:39 +0100

    Removed useless inline style

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -9,10 +9,7 @@ export type NavbarProps = {
 export default function Navbar({showLogoutButton=false, onLogout=() => {}}: NavbarProps) {
     return <nav>
       <a className="logo" href="/">
-        <img className="logo-img" src={logo} style={{
-          background: "radial-gradient(circle at center, #fff5 0, var(--bs-body-bg) 50%, var(--bs-body-bg) 100%)",
-          borderRadius: "50%"
-        }} />
+        <img className="logo-img" src={logo} />
         <h1>NixCraft</h1>
       </a>
       <button className={`primary-button logout-button ${!showLogoutButton ? 'hidden' : ''}`} type="button" onClick={onLogout}>Logout</button>
